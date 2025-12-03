cbde42b1609e753cc2dfce9d9771f7a754360b8a
jrahul12 <rj0093716@gmail.com>
2025-12-03 18:03:10 +0530

    fetch the all data from db

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,37 +8,66 @@ const addBtn = document.getElementById('addBtn');
 const updateBtn = document.getElementById('updateBtn');
 const blogContainer = document.getElementById('blogContainer');
 
-let BASE_URL="https://blog-80452-default-rtdb.firebaseio.com";
+let BASE_URL = "https://blog-80452-default-rtdb.firebaseio.com";
 
-let POST_URL=`${BASE_URL}/blog.json`;
+let POST_URL = `${BASE_URL}/blog.json`;
 
-function blogObjToArr(obj){
-    let arr=[];
+function blogObjToArr(obj) {
+    let arr = [];
     for (const key in obj) {
-        obj[key].id=key;
-        arr.push(key[obj]);
+        obj[key].id = key;
+        arr.push(obj[key]);
     }
     return arr;
 }
 
-async function makeApiCall(apiurl,methodName,msgBody) {
-    try{
-        msgBody=msgBody?JSON.stringify(msgBody):null;
-        let res = await fetch(apiurl,{
-            method : methodName,
-            body : msgBody,
-            headers:{
-                "Auth":"token from Ls",
-                "Content-type":"application/json"
+async function makeApiCall(apiurl, methodName, msgBody) {
+    try {
+        msgBody = msgBody ? JSON.stringify(msgBody) : null;
+        let res = await fetch(apiurl, {
+            method: methodName,
+            body: msgBody,
+            headers: {
+                "Auth": "token from Ls",
+                "Content-type": "application/json"
             }
         })
-        let data=await res.json();
+        let data = await res.json();
         if (!res.ok) {
-            let err=data.error||res.statusText||"Something Went Wrong";
+            let err = data.error || res.statusText || "Something Went Wrong";
             throw new Error(err);
         }
         return data;
-    }finally{
-        
+    } finally {
+
     }
 }
+
+const createBlog = arr => {
+    let result = arr.map(blog => {
+        return `<div class="card" id="${blog.id}">
+                        <div class="card-header">
+                            <h3>${blog.title}</h3>
+                        </div>
+                        <div class="card-body">
+                            <p>${blog.body}</p>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between">
+                            <button class="btn btn-sm btn-block btn-success">Edit</button>
+                            <button class="btn btn-sm btn-block btn-danger">Delete</button>
+                        </div>
+                    </div>`
+    }).join('');
+    blogContainer.innerHTML = result;
+}
+
+async function fetchAllBlog(eve) {
+    try {
+        let data = await makeApiCall(POST_URL, "GET", null)
+        let blogArr = blogObjToArr(data)
+        createBlog(blogArr);
+    } catch(err) {
+        cl(err)
+    }
+}
+fetchAllBlog();
