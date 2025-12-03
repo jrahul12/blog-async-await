38579fa0728d910e408a73bbf1febc522bdc5025
jrahul12 <rj0093716@gmail.com>
2025-12-03 17:49:41 +0530

    created generic function

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,24 @@ function blogObjToArr(obj){
     return arr;
 }
 
+async function makeApiCall(apiurl,methodName,msgBody) {
+    try{
+        msgBody=msgBody?JSON.stringify(msgBody):null;
+        let res = await fetch(apiurl,{
+            method : methodName,
+            body : msgBody,
+            headers:{
+                "Auth":"token from Ls",
+                "Content-type":"application/json"
+            }
+        })
+        let data=await res.json();
+        if (!res.ok) {
+            let err=data.error||res.statusText||"Something Went Wrong";
+            throw new Error(err);
+        }
+        return data;
+    }finally{
+        
+    }
+}
